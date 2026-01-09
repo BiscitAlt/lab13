3d36e7acb3d3d16d0bcc907d62488a9729bf17c0
BiscitAlt <thitiputt.pe@gmail.com>
2026-01-09 23:53:31 +0700

    lab13_1 completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,24 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double num[],int count,double result[]){
+    double sum,sd,hm[count],sumhm;
+    result[2] = 1.00;
+    result[5] += num[0];
+    for (int i = 0; i < count; i++){
+        sum += num[i];
+        result[2] *= num[i];
+        hm[i] = 1/num[i];
+    }
+    result[0] = sum/count;
+    result[2] = pow(result[2],(double)1/count);
+    for (int i = 0; i < count; i++){
+        sd += pow(num[i]-result[0],2);
+        sumhm += hm[i];
+        if(result[4] < num[i]) result[4] = num[i];
+        if(result[5] > num[i]) result[5] = num[i];
+    }
+    result[1] = sqrt(sd/count);
+    result[3] = count/sumhm;
+}
